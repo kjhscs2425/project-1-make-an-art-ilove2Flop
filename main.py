dc6a6e7815b67c71952a3e058fbcd510daa49d3d
floppa <jbarek2027@gmail.com>
2024-10-09 12:08:26 -0700

    color

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ my_turtle.speed(300)
 w = 5 #W can be all real numbers
 for _ in range(200):
     r = 1+w #R can be all real numbers
+    my_turtle.color("red")
     my_turtle.circle(r+1) 
     w = r+3
 
@@ -12,6 +13,7 @@ for _ in range(200):
 x = 2
 for _ in range(300):
     y = 2+x #Y has to be greater than x
+    my_turtle.color("green")
     my_turtle.left(140) 
     my_turtle.forward(1+y) 
     x=y+2
@@ -22,6 +24,7 @@ for _ in range(100):
         t.speed(300) 
 
         for i in range(size):
+            t.color("blue")
             t.forward(i)
             t.right(angle)
 
@@ -33,7 +36,6 @@ for _ in range(100):
 def circular_spiral_upside_down(radius=10, step=5, rotations=10):
         t = turtle.Turtle()
         t.speed(0)  # Set the turtle's speed to the fastest
-  
 for i in range(rotations * 360):
     t.forward(radius)
     t.left(90)
